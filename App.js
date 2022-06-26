c452bdca4fa5f2cf173d77fa71c537c19c1a0a6a
Surajmahato <surajmahato1159@gmail.com>
2022-06-26 13:53:48 +0100

    image and text are displayed correctly

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-
+import CarItem from './assets/components/CarItem'
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Text>here you are</Text> */}
+      <CarItem />
       <StatusBar style="auto" />
     </View>
   )
---
+++ assets/components/CarItem/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+
+  titles: {
+    margin: '30%',
+    alignItems: 'center',
+  },
+  title: {
+     fontSize: 40,
+     fontWeight: '500',
+ },
+   subtitle: {
+     fontSize: 16,
+     color: '#5c5e62',
+},
+   image: {
+     height: '100%',
+     width: '100%',
+     position: 'absolute',
+     resizeMode: 'cover',
+ },
+})
+export default styles
